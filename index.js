9e4430673952991c20c05b5d0ac89c56fbadb6a8
Kaushal Krishna <132030841+kaushal-krishna@users.noreply.github.com>
2024-04-01 10:28:27 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,60 @@
+import express from "express";
+import cors from "cors";
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const PORT = 4000;
+const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "https://flexomate.web.app",
+        methods: ["GET", "POST"],
+        credentials: true,
+    },
+});
+
+app.use(
+    cors({
+        origin: "https://flexomate.web.app",
+        methods: ["GET", "POST"],
+        credentials: true,
+    })
+);
+
+app.get("/", (req, res) => {
+    res.send({
+        message: "The server is live...",
+    });
+});
+
+io.on("connection", (socket) => {
+    console.log("Socket Connected :", socket.id);
+
+    socket.on("join-rooms", (roomIds, socketId, username) => {
+        roomIds.forEach((roomId) => {
+            socket.join(roomId);
+        });
+        console.log(`${socketId} : User ${username} joined all Rooms`);
+    });
+
+    socket.on("send-message", (currentRoomId, socketId, username, message) => {
+        console.log({
+            currentRoomId,
+            socketId,
+            username,
+            message,
+        });
+        socket.broadcast.to(currentRoomId).emit("receive-message", username, message);
+        console.log("Socket IDs in room :", Array.from(io.sockets.adapter.rooms.get(currentRoomId) || []));
+    });
+
+
+    socket.on("disconnect", () => {
+        console.log("User Disconnected", socket.id);
+    });
+});
+
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
