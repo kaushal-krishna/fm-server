ff3683045a8e017bb38d69099ec43d508c57462f
Kaushal Krishna <132030841+kaushal-krishna@users.noreply.github.com>
2024-04-02 10:06:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: ["http://localhost","http://localhost:3000", "https://flexomate.web.app"],,
+        origin: ["http://localhost","http://localhost:3000", "https://flexomate.web.app"],
         methods: ["GET", "POST"],
         credentials: true,
     },
@@ -16,7 +16,7 @@ const io = new Server(server, {
 
 app.use(
     cors({
-        origin: ["http://localhost","http://localhost:3000", "https://flexomate.web.app"],,
+        origin: ["http://localhost","http://localhost:3000", "https://flexomate.web.app"],
         methods: ["GET", "POST"],
         credentials: true,
     })
